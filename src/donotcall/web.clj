c8fa0efb9cb60b532f8104e9e4722bb6fe1f7384
Sam Davies <seivadmas@gmail.com>
2015-04-06 09:54:01 -0300

    Ridiculously convoluted effort to access DATABASE_URL from within Tomcat

--- src/donotcall/web.clj
+++ src/donotcall/web.clj
@@ -4,14 +4,18 @@
             [ring.adapter.jetty :as ring]
             [alex-and-georges.debug-repl :refer :all]
             [clojure.java.jdbc :as sql])
+  (:import [javax.naming Context InitialContext])
   (:gen-class))
 
-(def db (atom "postgresql://localhost:5432/donotcall")) ;; define it for development
+(def db
+  (try
+    (. (. (InitialContext.) lookup "java:comp/env") lookup "DATABASE_URL")
+    (catch javax.naming.NoInitialContextException e "postgresql://localhost:5432/donotcall"))) ;; define it for development
 
 (defn number-exists?
   "Returns the number as a string if it exists, otherwise return false"
   [number]
-  (let [result (first (sql/query @db ["SELECT number FROM do_not_call_phones WHERE number = ?" number]))]
+  (let [result (first (sql/query db ["SELECT number FROM do_not_call_phones WHERE number = ?" number]))]
     (if result
       {number :number}
       false)))
@@ -23,9 +27,8 @@
       "{}"))
 
 (defroutes routes
-  (GET "/donotcall/:number" [number] (check number)))
+  (GET "/donotcall/:number" [number] (check number))
+  (GET "/debuginfo" [] (str "db: " db "\nDATABASE_URL: " (. (. (InitialContext.) lookup "java:comp/env") lookup "DATABASE_URL"))))
 
 (defn -main []
-  (if-let [database-url (System/getenv "DATABASE_URL")]
-    (reset! db database-url))
   (ring/run-jetty #'routes {:port 3001 :join? false}))
